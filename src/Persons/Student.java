2b8fd550746204e53c3d5517850aa9b62cbe4ddd
Dennis Marchena <demarchenade@est.utn.ac.cr>
2025-07-04 15:54:56 -0600

    Se creo clase Student
    Se crea clase Student  dentro del Package Persons, la cual, contiene 1 atributos, Deparment, teniendo estos un metodo get cada uno y set, y un metodo constructor

---
+++ src/Persons/Student.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Persons;
+
+/**
+ * 04/07/2025 ult mod: 2:21pm
+ * @author Dennis Marchena
+ */
+public class Student extends Person {
+private String Career;    
+
+//Metodos Get
+public String getCarrer() {
+    return this.Career;
+ }
+
+//Metodos Set
+public void setCarrer(String Career) {
+    this.Career=Career;
+ }
+
+//Constructor
+public void Student (String Career, String Id, String Name) {
+super(String Id, String Name);
+this.Career=Career;
+ }
+
+}
