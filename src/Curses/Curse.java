05b81270c3bf6014a8bef2348424b64ed2168f57
Dennis Marchena <demarchenade@est.utn.ac.cr>
2025-07-04 16:03:20 -0600

    Se creo clase Curse
    Se crea clase Curse  dentro del Package Curses, la Clase Curser  contiene 3 atributos, siendo Code, Name y Group, teniendo estos un metodo get cada uno

---
+++ src/Curses/Curse.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Curses;
+
+/**
+ * 04/07/2025 ult mod: 2:21pm
+ * @author Dennis Marchena
+ */
+public class Curse {
+    
+}
